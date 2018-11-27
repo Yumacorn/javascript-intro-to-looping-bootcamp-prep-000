48e0fc1425e01e72f965a590f7b491e50909782e
Ryan Mui <reachryanmui@gmail.com>
2018-11-27 22:52:35 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,7 +1,12 @@
 /*for (var i = 1; i < 100; i++) {
   console.log("Hello World the " + i + " time!")
+<<<<<<< HEAD
 }
 */
+=======
+}*/
+
+>>>>>>> 234328dbe0259d0c709a8dec0892b49f8fdf62f8
 function forLoop(array) {
   for(let i = 0; i < 25; i++) {
     if(i===1){
@@ -10,13 +15,19 @@ function forLoop(array) {
     else {
       array.push(`I am ${i} strange loops.`)
     }
+<<<<<<< HEAD
   }
   console.log("forLoop run!")
   return array
+=======
+    return array
+  }
+>>>>>>> 234328dbe0259d0c709a8dec0892b49f8fdf62f8
 }
 
 var box = []
 forLoop(box)
+<<<<<<< HEAD
 //console.log(box)
 
 function whileLoop(n) {
@@ -45,3 +56,6 @@ console.log(array)
 console.log("were done")
 return array
 }
+=======
+console.log(box)
+>>>>>>> 234328dbe0259d0c709a8dec0892b49f8fdf62f8
